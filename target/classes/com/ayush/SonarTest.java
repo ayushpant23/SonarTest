995c6fb7d37fd771987c9d3476ed26c738e6aa38
aypant-capgeminidotcom <ayush.pant@capgemini.com>
2020-05-20 19:18:09 +0530

    New Commit

--- target/classes/com/ayush/SonarTest.java
+++ target/classes/com/ayush/SonarTest.java
@@ -2,6 +2,11 @@ package com.ayush;
 
 public class SonarTest {
 	public static void main(String[] args) {
-		String init = "App Init";
+		String init = "App Initilization is in progress";
+		AppInit(init);
+	}
+	
+	public static void AppInit(String msg) {
+		System.out.println(msg);
 	}
 }
